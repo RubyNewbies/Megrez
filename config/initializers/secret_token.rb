c026b22a1471061be5f42a60ea485a303dfea4bb
sandancient <sandancient@gmail.com>
2014-10-11 17:08:06 +0800

    origin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Megrez::Application.config.secret_token = 'd6dc6e8f2da12b4d7a640da4f13db8cb257f3098e328de0239d65f089b1c912443b127ec80f542abe708a6926da4e78aa3cec6726d95c10e954dad0389e7e3a6'
