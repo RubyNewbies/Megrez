8f99af5eeb420a4f838368e0d01a2b62e119d6eb
DeathKing <deathking0622@gmail.com>
2014-10-21 23:32:08 +0800

    finished simple regist and auth.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  include UsersHelper
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by_username(params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
+    else
+      flash[:error] = '账号/密码错误！'
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out if signed_in?
+    redirect_to root_path
+  end
+
+end
