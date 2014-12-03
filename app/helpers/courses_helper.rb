aea3b0603861346ae3ec2aa2d37fad18264c9e36
DeathKing <deathking0622@gmail.com>
2014-12-03 22:38:46 +0800

    simple ajax improvement.

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -12,12 +12,4 @@ module CoursesHelper
       course.nodes.map {|n| [n.name, n.id] }
   end
 
-  def ajax_node_to(*arg)
-    arg[2] ||= {}
-    #arg[2]['data-node-id'] = params[:id]
-    arg[2]['data-ajax-node-link'] = true
-    arg[2][:remote] = true
-    link_to(*arg)
-  end
-
 end
