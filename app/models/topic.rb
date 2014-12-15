a3ec021069e23bf460cfd6aa175540276d4ca6fd
DeathKing <deathking0622@gmail.com>
2014-12-15 23:46:02 +0800

    simple beatufy forum page.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,8 +4,8 @@ class Topic < ActiveRecord::Base
 
   validates :title, length: { in: 1..120, too_short: "标题至少%{count}个字符", too_long: "标题至多%{count}个字符" }
 
-  has_one   :user
   has_many  :replies
+  belongs_to  :user
   belongs_to  :node
 
   before_save :render_body
