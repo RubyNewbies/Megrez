d1ba2d9741dae3c81ad70753f1602b7575da1164
steveyang94 <yangshiqi1q2w3e@gmail.com>
2014-12-17 00:12:21 +0800

    some bugs fixed

--- app/models/user_file.rb
+++ app/models/user_file.rb
@@ -1,5 +1,5 @@
 class UserFile < ActiveRecord::Base
-  has_attached_file :attachment
+  has_attached_file :attachment, :restricted_characters => RESTRICTED_CHARACTERS
   do_not_validate_attachment_file_type :attachment
 
   belongs_to :folder
