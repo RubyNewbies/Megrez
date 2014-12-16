e468c308d92f62d12c185148dbbf6b7976b5e291
steveyang94 <yangshiqi1q2w3e@gmail.com>
2014-12-16 21:53:55 +0800

    bug fixed; course icon

--- db/schema.rb
+++ db/schema.rb
@@ -205,8 +205,8 @@ ActiveRecord::Schema.define(version: 20141216124212) do
     t.string   "username"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "remember_token"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
