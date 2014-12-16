2a8983b423fad2c2d20c6c625cc2341e73f51744
DeathKing <deathking0622@gmail.com>
2014-12-16 21:33:18 +0800

    topic mentioned.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,7 @@
 class Topic < ActiveRecord::Base
 
   include TopicsHelper
+  include Rails.application.routes.url_helpers
 
   validates :title, length: { in: 1..120  }
 
@@ -9,18 +10,24 @@ class Topic < ActiveRecord::Base
   belongs_to  :node
 
   before_save :render_body
-  #after_save :send_notifications
+  after_save :send_notifications
 
   # Credit for this regular expression
   # https://github.com/daqing/rabel/blob/master/app/models/notifiable.rb
   MENTION_REGEXP = /@([a-zA-Z0-9_\-\p{han}]+)/u
 
   def render_body
+    self.body = self.source
+    self.body.gsub!(MENTION_REGEXP) do |match|
+      if u = User.find_by_username(match[1..-1])
+        ActionController::Base.helpers.link_to match, profile_path(username: match[1..-1])
+      end
+    end
     self.body = markdown(source).html_safe
   end
 
   def mentioned_users
-    mentioned_names = body.scan(MENTION_REGEXP).collect {|matched| matched.first}.uniq
+    mentioned_names = source.scan(MENTION_REGEXP).collect {|matched| matched.first}.uniq
     mentioned_names.delete(user.username)
     mentioned_names.map { |name| User.where(username: name).first }.compact
   end
@@ -32,12 +39,12 @@ class Topic < ActiveRecord::Base
   private
 
   def send_notifications
-    user = User.find(user_id)
-    user_url = "<a href=\"/u/#{user.username}\">#{user.username}</a>"
-    mentioned_users.each do |user|
-      notification = "#{user_url}在#{title}中提到了你。"
-      puts "Create notification: " + notification
-      Notification.create(user_id: topic.user.id, content: notification)
+    username = "@#{user.username}(#{user.realname})"
+    user_url = ActionController::Base.helpers.link_to username, profile_path(username: user.username)
+    message = "#{user_url}在#{title}中提到了你。"
+    target_url = course_topic_path(course_id: node.course.id, id: id)
+    mentioned_users.each do |u|
+      Notification.create(user_id: u.id, content: message, target_url: target_url)
     end
   end
 
